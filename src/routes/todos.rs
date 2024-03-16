592c3af7cf781aeab63975634dfc78138649a95b
Benoit Jeaurond <benoit@jeaurond.dev>
2024-03-16 17:41:10 -0400

    feat: add bulk actions on todos

--- src/routes/todos.rs
+++ src/routes/todos.rs
@@ -1,4 +1,9 @@
-use axum::{extract::State, response::IntoResponse, routing::get, Form, Router};
+use axum::{
+    extract::State,
+    response::IntoResponse,
+    routing::{delete, get, put},
+    Form, Router,
+};
 use serde::Deserialize;
 
 use crate::{
@@ -7,7 +12,10 @@ use crate::{
 };
 
 pub fn router() -> Router<AppState> {
-    Router::new().route("/", get(get_todo).post(add_todo))
+    Router::new()
+        .route("/", get(get_todo).post(add_todo).delete(delete_all_todos))
+        .route("/done", delete(delete_done_todos).put(mark_all_done))
+        .route("/undone", put(mark_all_undone))
 }
 
 async fn get_todo(State(state): State<AppState>) -> templating::TodosTemplate {
@@ -35,4 +43,44 @@ async fn add_todo(State(state): State<AppState>, Form(todo): Form<AddTodo>) -> i
     state.alert_stats();
 
     templating::AddTodoTemplate { todo }
+}
+
+async fn delete_all_todos(State(state): State<AppState>) -> impl IntoResponse {
+    state.todos.write().await.clear();
+    state.alert_stats();
+
+    templating::TodosTemplate::default()
+}
+
+async fn delete_done_todos(State(state): State<AppState>) -> impl IntoResponse {
+    let mut todos = state.todos.write().await;
+
+    todos.retain(|todo| !todo.done);
+    state.alert_stats();
+
+    templating::TodosTemplate {
+        todos: todos.to_vec(),
+    }
+}
+
+async fn mark_all_done(State(state): State<AppState>) -> impl IntoResponse {
+    set_todos_done(state, true).await
+}
+
+async fn mark_all_undone(State(state): State<AppState>) -> impl IntoResponse {
+    set_todos_done(state, false).await
+}
+
+async fn set_todos_done(state: AppState, done: bool) -> impl IntoResponse {
+    let mut todos = state.todos.write().await;
+
+    for todo in todos.iter_mut() {
+        todo.done = done;
+    }
+
+    state.alert_stats();
+
+    templating::TodosTemplate {
+        todos: todos.to_vec(),
+    }
 }
