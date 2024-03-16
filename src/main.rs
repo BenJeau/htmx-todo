533c4f57b0913dc5e2c69e570f926d784db8827e
Benoit Jeaurond <benoit@jeaurond.dev>
2024-03-16 13:41:07 -0400

    feat: serve static files with axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod routes;
+
+#[tokio::main]
+async fn main() {
+    let router = routes::router();
+
+    let addr = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    tracing::info!("listening on {addr}");
+    axum::serve(listener, router).await.unwrap();
 }
