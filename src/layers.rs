32be59c1ce7d747f0f72ebc04adbf6bd75fc647a
Benoit Jeaurond <benoit@jeaurond.dev>
2024-03-16 17:57:31 -0400

    feat: add html minification layer

--- src/layers.rs
+++ src/layers.rs
@@ -1,8 +1,12 @@
 use axum::{
+    body::Body,
     extract::{DefaultBodyLimit, Request},
     http::Method,
+    middleware::{self, Next},
+    response::Response,
     Router,
 };
+use minify::html::minify;
 use tower_http::{
     compression::{
         predicate::{NotForContentType, SizeAbove},
@@ -36,8 +40,37 @@ pub fn apply_layers(router: Router) -> Router {
     let size_limit_layer = DefaultBodyLimit::max(1024 * 1024);
 
     router
+        .layer(middleware::from_fn(minify_html_response))
         .layer(compression_layer)
         .layer(size_limit_layer)
         .layer(cors_layer)
         .layer(tracing_layer)
+}
+
+async fn minify_html_response(request: Request, next: Next) -> Response<Body> {
+    let response = next.run(request).await;
+
+    let Some(content_type) = response.headers().get("content-type") else {
+        return response;
+    };
+
+    if !content_type.to_str().unwrap().contains("text/html") {
+        return response;
+    }
+
+    let (mut parts, body) = response.into_parts();
+
+    return axum::body::to_bytes(body, usize::MAX)
+        .await
+        .map(|b| minify(String::from_utf8_lossy(&b).as_ref()))
+        .map(|b| {
+            let content_length = b.len();
+
+            parts
+                .headers
+                .insert("content-length", content_length.into());
+
+            Response::from_parts(parts, Body::from(b))
+        })
+        .unwrap();
 }
